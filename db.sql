bffaaa040789552326221d692ca5fdcb00a2e415
WARDANADANN <wardanadann@gmail.com>
2023-03-30 19:19:16 +0700

    modify db.sql

--- db.sql
+++ db.sql
@@ -1,40 +1,71 @@
-CREATE TABLE role 
-(
-    roleId varchar(255),
-    roleName varchar(255),
-    roleLevel varchar(255),
-    primary key(roleId)
+-- CREATE TABLE role 
+-- (
+--     roleId varchar(255),
+--     roleName varchar(255),
+--     roleLevel varchar(255),
+--     primary key(roleId)
+-- );
+CREATE TABLE roles (
+  role_id INT PRIMARY KEY auto_increment,
+  role_name VARCHAR(50),
+  role_level VARCHAR(25)
 );
 
 
 
-CREATE TABLE user 
-(
-    userId varchar(255),
-    userName varchar(255),
-    userNum varchar(255),
-    userEmail varchar(255),
-    userPassword varchar(255),
-    roleId varchar(255),
-    primary key(userId),
-    foreign key(roleId) references role(roleId)
+-- CREATE TABLE user 
+-- (
+--     userId varchar(255),
+--     userName varchar(255),
+--     userNum varchar(255),
+--     userEmail varchar(255),
+--     userPassword varchar(255),
+--     roleId varchar(255),
+--     primary key(userId),
+--     foreign key(roleId) references role(roleId)
+-- );
+CREATE TABLE users (
+  user_id INT auto_increment PRIMARY KEY,
+  user_name VARCHAR(255),
+  user_num VARCHAR(255),
+  user_email VARCHAR(255),
+  user_password VARCHAR(255),
+  role_id INT ,
+  FOREIGN KEY (role_id) REFERENCES roles (role_id)
+);
+ALTER TABLE users ADD COLUMN user_school VARCHAR(255);
+
+
+
+-- CREATE TABLE questions 
+-- (
+--     questionId varchar(255),
+--     questionName varchar(255),
+--     roleId varchar(255),
+--     primary key(questionId),
+--     foreign key(roleId) references role(roleId)
+-- );
+CREATE TABLE questions (
+  question_id int auto_increment PRIMARY KEY,
+  question_name VARCHAR(255),
+  role_id int,
+  FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
 
 
-CREATE TABLE questions 
-(
-    qId varchar(255),
-    qName varchar(255),
-    roleId varchar(255),
-    primary key(qID),
-    foreign key(roleId) references role(roleId)
+-- CREATE TABLE events 
+-- (
+--     eventId int auto_increment,
+--     eventname varchar(255),
+--     roleId varchar(255),
+--     event_description varchar(9999),
+--     primary key(eventID),
+--     foreign key(roleId) references role(roleId)
+-- )
+CREATE TABLE events (
+  event_id INT AUTO_INCREMENT PRIMARY KEY,
+  event_name VARCHAR(255),
+  role_id VARCHAR(255),
+  event_description VARCHAR(9999),
+  FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
-
-CREATE TABLE events 
-(
-    eventId varchar(255),
-    eventname varchar(255),
-    roleId varchar(255),
-    primary key(eventID),
-    foreign key(roleId) references role(roleId)
-)
---
+++ old_view/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <h1>LOGIN</h1>
+    <form action="" method="post">
+        <label>
+            username
+            <input type = "username" name = "username">
+        </label>
+        <label>
+            Email
+            <input type = "email" name = "email">
+        </label>
+        <button type="submit" name="login">LOGIN</button>
+    </form>
+</body>
+</html>
